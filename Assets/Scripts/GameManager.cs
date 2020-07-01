8cadd71259cef109468da2fe74a705203f62a668
infomuscle <infomuscle10@gmail.com>
2020-07-01 22:55:54 +0900

    Started to Develop Intro

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager gameManager
+    {
+        get
+        {
+            if (m_gameManager == null)
+            {
+                m_gameManager = FindObjectOfType<GameManager>();
+            }
+            return m_gameManager;
+        }
+    }
+
+    private static GameManager m_gameManager;
+    
+    
+    
+    private GameObject btnAdopt;
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
+        btnAdopt = GameObject.Find("ButtonAdopt");
         
     }
 
---
+++ Assets/Scripts/IntroManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class IntroManager : MonoBehaviour
+{
+    public Button btnAdopt;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+    }
+
+
+    public void btnTest()
+    {
+        Debug.Log("버튼이 눌렸다!");
+        Debug.Log("메인으로 가자!");
+        SceneManager.LoadScene("Main");
+    }
+}
